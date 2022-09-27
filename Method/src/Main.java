a4d9456de75b7d0f4da5bc43b2435b2d60d73b78
rbnpra <rbn.prajapati1@gmail.com>
2022-09-27 09:20:01 -0700

    coding excersis1

--- Method/src/Main.java
+++ Method/src/Main.java
@@ -68,15 +68,26 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int playerScore) {
-        if (playerScore >= 1000) {
-            return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
-            return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
-            return 3;
-        }else {
-            return 4;
+
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500 && playerScore < 1000) {
+//            return 2;
+//        } else if (playerScore >= 100 && playerScore < 500) {
+//            return 3;
+//        }else {
+//            return 4;
+//        }
+        int position = 4; // assuming position 4 will be returned
+        if(playerScore >= 1000) {
+            position = 1;
+        } else if (playerScore >= 500) {
+            position =2;
+        } else if (playerScore >= 100) {
+            position = 3;
         }
+        return position;
+
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
