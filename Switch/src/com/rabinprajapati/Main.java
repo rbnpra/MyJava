a30305dc0f78e69e82352afeb0f959ac47019b96
rbnpra <rbn.prajapati1@gmail.com>
2022-09-28 23:00:29 -0700

    created switch statement

---
+++ Switch/src/com/rabinprajapati/Main.java
@@ -0,0 +1,75 @@
+package com.rabinprajapati; // control flow statement
+
+public class Main {
+    public static void main(String[] args) {
+//        int value = 1;
+//        if(value == 1) {
+//            System.out.println("Value was 1");
+//        } else if(value == 2) {
+//            System.out.println("Value was 2");
+//        } else {
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        int switchValue = 5;
+
+        switch(switchValue) {
+            case 1:
+                System.out.println("value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            case 3: case 4: case 5:
+                System.out.println("was a 3, or a 4, or a 5");
+                System.out.println("Actually it was a " + switchValue); // interesting idea
+                break;
+
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+
+        // New Challenge !
+        // Create a new switch statement using char instead of int
+        // Create a new char variable
+        // Create a switch statement testing for
+        // A, B, C, D, or E
+        // display a message if any of these are found and then break
+        // Add a default which displays a message saying not found
+
+        char charValue = 'D';
+        switch (charValue) {
+            case 'A':
+                System.out.println("charValue was A");
+                break;
+
+            case 'B':
+                System.out.println("charValue was B");
+                break;
+            case 'C': case 'D': case 'E':
+                System.out.println("was a C, or a D, or an E");
+                System.out.println("Actually it was a " + charValue); // interesting idea
+                break;
+
+            default:
+                System.out.println("message saying not found");
+
+        }
+
+        String month = "April";
+        switch(month.toUpperCase()) {
+            case "January":
+                System.out.println("Jan");
+                break;
+            case "June":
+                System.out.println("Jun");
+                break;
+            default:
+                System.out.println("Not sure");
+        }
+
+    }
+}
