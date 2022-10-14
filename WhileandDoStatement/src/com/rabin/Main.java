dc937616cc4b21591ee3b47debdefe6290083e46
rbnpra <rbn.prajapati1@gmail.com>
2022-10-13 21:58:01 -0700

    created whileloop

---
+++ WhileandDoStatement/src/com/rabin/Main.java
@@ -0,0 +1,20 @@
+package com.rabin;
+
+public class Main {
+    public static void main(String[] args) {
+        int count = 1;
+        while(count != 6) {
+            System.out.println("Count value is " + count);
+
+            count++;
+        }
+        System.out.println("******************");
+
+        for(int i = 1; i<7; i++) {
+
+
+            System.out.println("Count value is " + i);
+        }
+
+    }
+}
